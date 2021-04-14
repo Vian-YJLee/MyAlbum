ca30b4544c4fd00ef2c537ba36c72d122a20d9f0
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-04-14 13:19:17 +0900

    Initial Commit

---
+++ MyAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAlbum
+//
+//  Created by LeeYongJin on 2021/04/14.
+//  Copyright © 2021 com.vianney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
