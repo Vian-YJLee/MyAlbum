ca27dbc0276d27a693f69e3875fd4ae33bd6cdc2
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-04-14 14:21:24 +0900

    remind learning Initial Commit

--- MyAlbum/ViewController.swift
+++ MyAlbum/ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func hello(_ sender: Any) {
+        let alert = UIAlertController(title: "Hello", message: "My remind learning App", preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
