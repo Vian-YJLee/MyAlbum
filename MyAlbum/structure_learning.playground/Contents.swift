6f21e8c96d0a7e8ccc44d051a76ee10ab7052c4a
Vian-YJLee <Vianney.yjlee@gmail.com>
2021-06-02 22:30:47 +0900

    3. Struct: Store     - 구조체 안에서 동작하는 함수 포함 작성(거리 계산 후 가능/불가능 메시지 출력)     - 편의점 정보를 Store 로 묶음 4. func printNearbyCVS 수정     - 변수 store1~3를 구조체 Store로 묶었기 때문에 CVSData 파라미터 값도 Store로 변경하여 튜플 구성     - 계산된 거리에 따라 배달 가능/불가능 여부 출력하는 코드 작성 (변수 isDeliverble )

--- MyAlbum/structure_learning.playground/Contents.swift
+++ MyAlbum/structure_learning.playground/Contents.swift
@@ -9,17 +9,28 @@ struct Location {
     let x: Int
     let y: Int
 }
-/*
+
+struct Store {
+    let loc: Location
+    let name: String
+    let deliveryRange = 2.0
+    
+    func isDeliverbleToMyLoc(userLoc: Location) -> Bool {
+        let distanceToStore = distance(current: userLoc, target: loc)
+        return distanceToStore < deliveryRange
+    }
+}
+// 각 변수 안에 흩어진 정보를 묶어서 의미단위로 편성할 수 있음
+/* 단순 정보의 나열에서 데이터가 가진 의미를 묶고 관계성을 갖는 형태로 구성 및 표현
  let store1 = (x: 3, y: 5, name: "CU")
  let store2 = (x: 4, y: 6, name: "GS25")
  let store3 = (x: 1, y: 7, name: "SevenEleven")
+ 
  */
 // store1-3 변수들은 파라미터가 3개에서 2개가 됨
-let store1 = (loc: Location(x: 3, y: 5), name: "CU")
-let store2 = (loc: Location(x: 4, y: 6), name: "GS25")
-let store3 = (loc: Location(x: 1, y: 7), name: "SevenEleven")
-
-
+let store1 = Store(loc: Location(x: 3, y: 5), name: "CU")
+let store2 = Store(loc: Location(x: 4, y: 6), name: "GS25")
+let store3 = Store(loc: Location(x: 1, y: 7), name: "SevenEleven")
 
 // 각 편의점 간 거리를 구하는 함수 (삼각함수)
 //---수정: 좌표를 받는 파라미터를 구조체 Location으로 변경
@@ -35,24 +46,32 @@ func distance (current: Location, target: Location) -> Double {
 // 내 위치를 받아서 distance 함수를 사용해 계산한 후 가장 가까운 값을 찾아 출력
 
 
-func printNearbyCVS (currentLocation: Location, CVSData: [(loc: Location, name: String)]) {
-    // storeData의 타입은 구조체
+func printNearbyCVS (currentLocation: Location, CVSData: [Store]) {
+    // 구조체 Store에 store1-3을 넣었기 때문에 CVSData는 Store로 튜플 구성
  
     var nearbyCVSName = ""
     var nearbyCVSDistance =  Double.infinity
+    var isDeliverble = false
+    
     
     for CVS in CVSData {
         let distanceToCVS = distance(current: currentLocation, target: CVS.loc)
         nearbyCVSDistance = min(distanceToCVS, nearbyCVSDistance)
         if nearbyCVSDistance == distanceToCVS {
             nearbyCVSName = CVS.name
+            isDeliverble = CVS.isDeliverbleToMyLoc(userLoc: currentLocation)
         }
     }
     print("가장 가까운 편의점은 \(nearbyCVSName) 입니다.")
     print("거리값 \(nearbyCVSDistance)")
+    if isDeliverble == false {
+        print("현재 위치까지는 배달이 불가합니다.")
+    } else {
+        print("현재 위치까지 배달 가능합니다.")
+    }
 }
 
-let myLocation = Location(x: 2, y: 9)
+let myLocation = Location(x: 3, y: 6)
 let stores = [store1, store2, store3]
 
 printNearbyCVS(currentLocation: myLocation, CVSData: stores)
